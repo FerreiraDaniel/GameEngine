8a29f9633418d954d1cd5bb2d4ee0b8a6ee9ab5e
danielferreira <daniel.dos.santos.ferreira@gmail.com>
2016-09-03 16:43:00 +0200

    Start developing terrain for ios

---
+++ GameEngine_IOS/GameEngine/Shaders/Terrains/TerrainShaderConstants.h
@@ -0,0 +1,13 @@
+//
+//  TerrainShaderConstants.h
+//  GameEngine
+//
+//  Created by Daniel ferreira on 03/09/2016.
+//  Copyright © 2016 Daniel ferreira. All rights reserved.
+//
+
+#ifndef TerrainShaderConstants_h
+#define TerrainShaderConstants_h
+
+
+#endif /* TerrainShaderConstants_h */
---
+++ GameEngine_IOS/GameEngine/Shaders/Terrains/TerrainShaderManager.m
@@ -0,0 +1,13 @@
+//
+//  TerrainShaderManager.m
+//  GameEngine
+//
+//  Created by Daniel ferreira on 03/09/2016.
+//  Copyright © 2016 Daniel ferreira. All rights reserved.
+//
+
+#import "TerrainShaderManager.h"
+
+@implementation TerrainShaderManager
+
+@end
