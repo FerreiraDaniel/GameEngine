b74617b1849f0f351a66bcd8d5f0328323124794
danielferreira <daniel.dos.santos.ferreira@gmail.com>
2016-10-03 22:28:20 +0200

    Migration to swift

--- GameEngine_IOS/GameEngine/Models/RawModel.swift
+++ GameEngine_IOS/GameEngine/Models/RawModel.swift
@@ -1,4 +1,9 @@
 import Foundation
 
 public class RawModel : RawModel1 {
+    
+    
+    public init!(vaoId: Int32, indicesData: UnsafeMutablePointer<UInt16>, indicesCount: Int32) {
+        super.init(vaoId, indicesData, indicesCount);
+    }
 }
