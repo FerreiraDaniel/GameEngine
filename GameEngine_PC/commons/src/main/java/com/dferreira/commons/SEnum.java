2fdca5382825e94255759da7fb9d0151e09e69a5
daniel <daniel@daniel-EasyNote-TR87>
2016-12-26 22:15:54 +0000

    Missing senum in previous commit

---
+++ GameEngine_PC/commons/src/main/java/com/dferreira/commons/SEnum.java
@@ -0,0 +1,12 @@
+package com.dferreira.commons;
+
+/**
+ * Interface that should be implemented by the enumerations
+ */
+public interface SEnum {
+
+	/**
+	 * @return The string value of the enumeration
+	 */
+	String getValue();
+}
