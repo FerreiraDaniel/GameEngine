cdf601962ca11665d2be1fd31ad120dd3525158b
danielferreira <daniel.dos.santos.ferreira@gmail.com>
2017-02-19 13:49:07 +0100

    Audio IOS small refactoring.

---
+++ GameEngine_IOS/GameEngine/AudioEngine/Decoder/AudioDecoder.swift
@@ -0,0 +1,122 @@
+import Foundation
+import AudioToolbox
+
+public class AudioDecoder {
+    
+    public init() {
+    }
+    
+    /**
+     * Get the data out of an audio file
+     *
+     * @param inFileURL The input url from which to read the audio file
+     * @return The data describing the audio thats been read
+     */
+    public func getData(inFileURL: NSURL) -> AudioData! {
+        var err: OSStatus = noErr
+        var theFileLengthInFrames: Int64 = 0
+        var theFileFormat: AudioStreamBasicDescription = AudioStreamBasicDescription()
+        var thePropertySize: UInt32 = UInt32(strideofValue(theFileFormat))
+        var extRef: ExtAudioFileRef = nil
+        var theData: UnsafeMutablePointer<CChar> = nil
+        var theOutputFormat: AudioStreamBasicDescription = AudioStreamBasicDescription()
+        
+        // Open a file with ExtAudioFileOpen()
+        err = ExtAudioFileOpenURL(inFileURL, &extRef)
+        if err != noErr
+        {
+            print("MyGetOpenALAudioData: ExtAudioFileOpenURL FAILED, Error = \(err)");
+            return nil
+        }
+        
+        // Get the audio data format
+        err = ExtAudioFileGetProperty(extRef, kExtAudioFileProperty_FileDataFormat, &thePropertySize, &theFileFormat)
+        if err != noErr
+        {
+            print("MyGetOpenALAudioData: ExtAudioFileGetProperty(kExtAudioFileProperty_FileDataFormat) FAILED, Error = \(err)");
+            return nil
+        }
+        if theFileFormat.mChannelsPerFrame > 2 {
+            print("MyGetOpenALAudioData - Unsupported Format, channel count is greater than stereo");
+            return nil
+        }
+        
+        // Set the client format to 16 bit signed integer (native-endian) data
+        // Maintain the channel count and sample rate of the original source format
+        theOutputFormat.mSampleRate = theFileFormat.mSampleRate
+        theOutputFormat.mChannelsPerFrame = theFileFormat.mChannelsPerFrame
+        
+        theOutputFormat.mFormatID = kAudioFormatLinearPCM
+        theOutputFormat.mBytesPerPacket = 2 * theOutputFormat.mChannelsPerFrame
+        theOutputFormat.mFramesPerPacket = 1
+        theOutputFormat.mBytesPerFrame = 2 * theOutputFormat.mChannelsPerFrame
+        theOutputFormat.mBitsPerChannel = 16
+        theOutputFormat.mFormatFlags = kAudioFormatFlagsNativeEndian | kAudioFormatFlagIsPacked | kAudioFormatFlagIsSignedInteger
+        
+        // Set the desired client (output) data format
+        err = ExtAudioFileSetProperty(extRef, kExtAudioFileProperty_ClientDataFormat, UInt32(strideofValue(theOutputFormat)), &theOutputFormat)
+        if err != noErr
+        {
+            print("MyGetOpenALAudioData: ExtAudioFileSetProperty(kExtAudioFileProperty_ClientDataFormat) FAILED, Error = \(err)");
+            return nil
+        }
+        
+        // Get the total frame count
+        thePropertySize = UInt32(strideofValue(theFileLengthInFrames))
+        err = ExtAudioFileGetProperty(extRef, kExtAudioFileProperty_FileLengthFrames, &thePropertySize, &theFileLengthInFrames)
+        if err != noErr
+        {
+            print("MyGetOpenALAudioData: ExtAudioFileGetProperty(kExtAudioFileProperty_FileLengthFrames) FAILED, Error = \(err)");
+            return nil
+        }
+        
+        // Read all the data into memory
+        let dataSize = UInt32(theFileLengthInFrames) * theOutputFormat.mBytesPerFrame
+        theData = UnsafeMutablePointer.alloc(Int(dataSize))
+        if theData == nil
+        {
+            return nil
+        }
+        else {
+            var theDataBuffer: AudioBufferList = AudioBufferList()
+            theDataBuffer.mNumberBuffers = 1
+            theDataBuffer.mBuffers.mDataByteSize = dataSize
+            theDataBuffer.mBuffers.mNumberChannels = theOutputFormat.mChannelsPerFrame
+            theDataBuffer.mBuffers.mData = UnsafeMutablePointer(theData)
+            
+            // Read the data into an AudioBufferList
+            var ioNumberFrames: UInt32 = UInt32(theFileLengthInFrames)
+            err = ExtAudioFileRead(extRef, &ioNumberFrames, &theDataBuffer)
+            if err == noErr {
+                // success
+                let audioData = AudioData()
+                audioData.channels = Int(theOutputFormat.mChannelsPerFrame)
+                audioData.data = theDataBuffer.mBuffers.mData
+                audioData.dataSize = Int32(dataSize)
+                audioData.rate = theOutputFormat.mSampleRate
+                return audioData
+            } else {
+                // failure
+                theData.dealloc(Int(dataSize))
+                theData = nil // make sure to return NULL
+                print("MyGetOpenALAudioData: ExtAudioFileRead FAILED, Error = \(err)");
+                return nil;
+            }
+        }
+    }
+    
+    
+    /**
+     *
+     *  Dispose the ExtAudioFileRef, it is no longer needed
+     */
+    public func cleanUp(audioData : AudioData!) {
+        if(audioData != nil) {
+            let theData = UnsafeMutablePointer<CChar>(audioData.data)
+            if theData != nil {
+                theData.dealloc(Int(audioData.dataSize))
+            }
+        }
+    }
+    
+}
--- GameEngine_IOS/GameEngine/RenderEngine/Loader.swift
+++ GameEngine_IOS/GameEngine/RenderEngine/Loader.swift
@@ -29,6 +29,11 @@ public class Loader : NSObject {
     private var textures : Array<GLuint>!;
     
     /**
+     * Decoder for audio files
+     */
+    private var audioDecoder : AudioDecoder;
+    
+    /**
      * Extension of the png files
      */
     private let PNG_EXTENSION : String  = "png";
@@ -42,6 +47,7 @@ public class Loader : NSObject {
         self.vaos = Array<GLuint>();
         self.vbos = Array<GLuint>();
         self.textures = Array<GLuint>();
+        self.audioDecoder = AudioDecoder()
     }
     
     /**
@@ -71,7 +77,7 @@ public class Loader : NSObject {
      */
     public func genAudioSources(numberOfSources : Int) -> [AudioSource] {
         var sourceLst : [AudioSource] = [AudioSource]();
-    
+        
         for _ in 0 ..< numberOfSources {
             let audioSource : AudioSource! = genAudioSource();
             if audioSource != nil {
@@ -380,84 +386,7 @@ public class Loader : NSObject {
     }
     
     
-    private func MyGetOpenALAudioData(inFileURL: NSURL, inout _ outDataSize: ALsizei, inout _ outDataFormat: ALenum, inout _ outSampleRate: ALsizei) -> UnsafeMutablePointer<Void> {
-        var err: OSStatus = noErr
-        var theFileLengthInFrames: Int64 = 0
-        var theFileFormat: AudioStreamBasicDescription = AudioStreamBasicDescription()
-        var thePropertySize: UInt32 = UInt32(strideofValue(theFileFormat))
-        var extRef: ExtAudioFileRef = nil
-        var theData: UnsafeMutablePointer<CChar> = nil
-        var theOutputFormat: AudioStreamBasicDescription = AudioStreamBasicDescription()
-        
-        Exit: do {
-            // Open a file with ExtAudioFileOpen()
-            err = ExtAudioFileOpenURL(inFileURL, &extRef)
-            if err != 0 { print("MyGetOpenALAudioData: ExtAudioFileOpenURL FAILED, Error = \(err)"); break Exit }
-            
-            // Get the audio data format
-            err = ExtAudioFileGetProperty(extRef, kExtAudioFileProperty_FileDataFormat, &thePropertySize, &theFileFormat)
-            if err != 0 { print("MyGetOpenALAudioData: ExtAudioFileGetProperty(kExtAudioFileProperty_FileDataFormat) FAILED, Error = \(err)"); break Exit }
-            if theFileFormat.mChannelsPerFrame > 2 { print("MyGetOpenALAudioData - Unsupported Format, channel count is greater than stereo"); break Exit }
-            
-            // Set the client format to 16 bit signed integer (native-endian) data
-            // Maintain the channel count and sample rate of the original source format
-            theOutputFormat.mSampleRate = theFileFormat.mSampleRate
-            theOutputFormat.mChannelsPerFrame = theFileFormat.mChannelsPerFrame
-            
-            theOutputFormat.mFormatID = kAudioFormatLinearPCM
-            theOutputFormat.mBytesPerPacket = 2 * theOutputFormat.mChannelsPerFrame
-            theOutputFormat.mFramesPerPacket = 1
-            theOutputFormat.mBytesPerFrame = 2 * theOutputFormat.mChannelsPerFrame
-            theOutputFormat.mBitsPerChannel = 16
-            theOutputFormat.mFormatFlags = kAudioFormatFlagsNativeEndian | kAudioFormatFlagIsPacked | kAudioFormatFlagIsSignedInteger
-            
-            // Set the desired client (output) data format
-            err = ExtAudioFileSetProperty(extRef, kExtAudioFileProperty_ClientDataFormat, UInt32(strideofValue(theOutputFormat)), &theOutputFormat)
-            if err != 0 { print("MyGetOpenALAudioData: ExtAudioFileSetProperty(kExtAudioFileProperty_ClientDataFormat) FAILED, Error = \(err)"); break Exit }
-            
-            // Get the total frame count
-            thePropertySize = UInt32(strideofValue(theFileLengthInFrames))
-            err = ExtAudioFileGetProperty(extRef, kExtAudioFileProperty_FileLengthFrames, &thePropertySize, &theFileLengthInFrames)
-            if err != 0 { print("MyGetOpenALAudioData: ExtAudioFileGetProperty(kExtAudioFileProperty_FileLengthFrames) FAILED, Error = \(err)"); break Exit }
-            
-            // Read all the data into memory
-            let dataSize = UInt32(theFileLengthInFrames) * theOutputFormat.mBytesPerFrame
-            theData = UnsafeMutablePointer.alloc(Int(dataSize))
-            if theData != nil {
-                var theDataBuffer: AudioBufferList = AudioBufferList()
-                theDataBuffer.mNumberBuffers = 1
-                theDataBuffer.mBuffers.mDataByteSize = dataSize
-                theDataBuffer.mBuffers.mNumberChannels = theOutputFormat.mChannelsPerFrame
-                theDataBuffer.mBuffers.mData = UnsafeMutablePointer(theData)
-                
-                // Read the data into an AudioBufferList
-                var ioNumberFrames: UInt32 = UInt32(theFileLengthInFrames)
-                err = ExtAudioFileRead(extRef, &ioNumberFrames, &theDataBuffer)
-                if err == noErr {
-                    // success
-                    outDataSize = ALsizei(dataSize)
-                    outDataFormat = (theOutputFormat.mChannelsPerFrame > 1) ? AL_FORMAT_STEREO16 : AL_FORMAT_MONO16
-                    outSampleRate = ALsizei(theOutputFormat.mSampleRate)
-                } else {
-                    // failure
-                    theData.dealloc(Int(dataSize))
-                    theData = nil // make sure to return NULL
-                    print("MyGetOpenALAudioData: ExtAudioFileRead FAILED, Error = \(err)"); break Exit;
-                }
-            }
-        }
-        
-        // Dispose the ExtAudioFileRef, it is no longer needed
-        if extRef != nil { ExtAudioFileDispose(extRef) }
-        return UnsafeMutablePointer(theData)
-    }
     
-    private func MyFreeOpenALAudioData(data: UnsafeMutablePointer<Void>, _ dataSize: ALsizei) {
-        let theData = UnsafeMutablePointer<CChar>(data)
-        if theData != nil {
-            theData.dealloc(Int(dataSize))
-        }
-    }
     
     /**
      * Load one audio file in a buffer
@@ -488,24 +417,34 @@ public class Loader : NSObject {
             print("Impossible to get the patch to audio \(fileName) ");
             return nil;
         }
-
-        var format: ALenum = 0
-        var size: ALsizei = 0
-        var freq: ALsizei = 0
+        
         
         let fileURL : NSURL = NSURL(fileURLWithPath: filePath)
         
-        var data: UnsafeMutablePointer<Void> = nil
-        data = MyGetOpenALAudioData(fileURL, &size, &format, &freq)
-
+        var audioFile : AudioData! = nil
         
-        alBufferData(bufferId, format, data, size, freq)
         
-        audioBuffer = AudioBuffer(ALint(bufferId));
+        audioFile = self.audioDecoder.getData(fileURL)
         
-        MyFreeOpenALAudioData(data, size)
-        return audioBuffer;
-
+        if(audioFile == nil)
+        {
+            return nil
+        } else {
+            //data = MyGetOpenALAudioData(fileURL, &size, &format, &freq)
+            
+            let format : ALenum = (audioFile.channels > 1) ? AL_FORMAT_STEREO16 : AL_FORMAT_MONO16
+            
+            
+            alBufferData(bufferId, format, audioFile.data, audioFile.dataSize, ALsizei(audioFile.rate))
+            self.audioDecoder.cleanUp(audioFile);
+            
+            audioBuffer = AudioBuffer(ALint(bufferId));
+            
+            return audioBuffer;
+        }
+        
+        
+        
         
         
         
